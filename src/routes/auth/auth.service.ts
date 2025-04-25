caaf1551796bfd3b93ba4e5ea2eb6a0814a3046a
Le Duc Thai <leducthai2001@gmail.com>
2025-04-25 09:24:50 +0700

    handle auth route

---
+++ src/routes/auth/auth.service.ts
@@ -0,0 +1,120 @@
+import { ConflictException, Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common'
+import { RolesService } from 'src/routes/auth/roles.service'
+import { isRecordNotFoundPrismaError, isUniqueConstraintPrismaError } from 'src/shared/helpers'
+import { HashingService } from 'src/shared/services/hashing.service'
+import { PrismaService } from 'src/shared/services/prisma.service'
+import { TokenService } from 'src/shared/services/token.service'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly hashingService: HashingService,
+    private readonly prismaService: PrismaService,
+    private readonly tokenService: TokenService,
+    private readonly rolesService: RolesService
+  ) {}
+  async register(body: any) {
+    try {
+      const clientRoleId = await this.rolesService.getClientRoleId()
+      const hashedPassword = await this.hashingService.hashPassword(body.password)
+      const user = await this.prismaService.user.create({
+        data: {
+          email: body.email,
+          password: hashedPassword,
+          name: body.name,
+          phoneNumber: body.phoneNumber,
+          roleId: clientRoleId
+        },
+        omit: {
+          password: true,
+          totpSecret: true
+        }
+      })
+      return user
+    } catch (error) {
+      if (isUniqueConstraintPrismaError(error)) {
+        throw new ConflictException('Email is already exist')
+      }
+      throw error
+    }
+  }
+
+  async login(body: any) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: body.email
+      }
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('Account is not exist')
+    }
+
+    const isPasswordMatch = await this.hashingService.comparePassword(body.password, user.password)
+    if (!isPasswordMatch) {
+      throw new UnprocessableEntityException([
+        {
+          field: 'password',
+          error: 'Password is incorrect'
+        }
+      ])
+    }
+    const tokens = await this.generateTokens({ userId: user.id })
+    return tokens
+  }
+
+  async generateTokens(payload: { userId: number }) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.tokenService.signAccessToken(payload),
+      this.tokenService.signRefreshToken(payload)
+    ])
+    const decodedRefreshToken = await this.tokenService.verifyRefreshToken(refreshToken)
+    await this.prismaService.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: payload.userId,
+        expiresAt: new Date(decodedRefreshToken.exp * 1000)
+      }
+    })
+    return { accessToken, refreshToken }
+  }
+
+  async refreshToken(refreshToken: string) {
+    try {
+      const { userId } = await this.tokenService.verifyRefreshToken(refreshToken)
+      await this.prismaService.refreshToken.findUniqueOrThrow({
+        where: {
+          token: refreshToken
+        }
+      })
+      await this.prismaService.refreshToken.delete({
+        where: {
+          token: refreshToken
+        }
+      })
+      return await this.generateTokens({ userId })
+    } catch (error) {
+      if (isRecordNotFoundPrismaError(error)) {
+        throw new UnauthorizedException('Refresh token has been revoked')
+      }
+      throw new UnauthorizedException()
+    }
+  }
+
+  async logout(refreshToken: string) {
+    try {
+      await this.tokenService.verifyRefreshToken(refreshToken)
+      await this.prismaService.refreshToken.delete({
+        where: {
+          token: refreshToken
+        }
+      })
+      return { message: 'Logout successfully' }
+    } catch (error) {
+      if (isRecordNotFoundPrismaError(error)) {
+        throw new UnauthorizedException('Refresh token has been revoked')
+      }
+      throw new UnauthorizedException()
+    }
+  }
+}
