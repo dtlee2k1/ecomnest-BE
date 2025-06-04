a4da8213db14ee753ce3c7e5c9ca761f5b5409ec
Le Duc Thai <leducthai2001@gmail.com>
2025-06-04 11:03:12 +0700

    migrate Order & ProductSKUSnapshot

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,16 @@
+import { ProductTranslationType } from 'src/shared/models/shared-product-translation.model'
 import { VariantsType } from 'src/shared/models/shared-product.model'
 
 /* eslint-disable @typescript-eslint/no-namespace */
 declare global {
   namespace PrismaJson {
     type VariantsCustom = VariantsType
+    type ProductTranslations = Pick<ProductTranslationType, 'id' | 'name' | 'description' | 'languageId'>[]
+    type Receiver = {
+      name: string
+      phone: string
+      address: string
+    }
   }
 }
 
