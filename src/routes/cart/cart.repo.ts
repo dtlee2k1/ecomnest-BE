7a6ea648dd715996e8ef44ac70cbdaebf7df56d3
Le Duc Thai <leducthai2001@gmail.com>
2025-06-04 16:58:42 +0700

    fixed bug add same product in cart

--- src/routes/cart/cart.repo.ts
+++ src/routes/cart/cart.repo.ts
@@ -17,7 +17,7 @@ import { PrismaService } from 'src/shared/services/prisma.service'
 export class CartRepo {
   constructor(private readonly prismaService: PrismaService) {}
 
-  private async validateSKU(skuId: number): Promise<SKUSchemaType> {
+  private async validateSKU(skuId: number, quantity: number): Promise<SKUSchemaType> {
     const sku = await this.prismaService.sKU.findUnique({
       where: { id: skuId, deletedAt: null },
       include: {
@@ -29,7 +29,7 @@ export class CartRepo {
       throw NotFoundSKUException
     }
 
-    if (sku.stock < 1) {
+    if (sku.stock < 1 || sku.stock < quantity) {
       throw OutOfStockSKUException
     }
     const { product } = sku
@@ -183,7 +183,7 @@ export class CartRepo {
                 ), '[]'::json)
               )
            )
-         )
+         ) ORDER BY "CartItem"."updatedAt" DESC
        ) AS "cartItems",
        jsonb_build_object(
          'id', "User"."id",
@@ -217,19 +217,30 @@ export class CartRepo {
   }
 
   async create(userId: number, body: AddToCartBodyType): Promise<CartItemType> {
-    await this.validateSKU(body.skuId)
+    await this.validateSKU(body.skuId, body.quantity)
 
-    return this.prismaService.cartItem.create({
-      data: {
+    return this.prismaService.cartItem.upsert({
+      where: {
+        userId_skuId: {
+          userId,
+          skuId: body.skuId
+        }
+      },
+      create: {
         userId,
         skuId: body.skuId,
         quantity: body.quantity
+      },
+      update: {
+        quantity: {
+          increment: body.quantity
+        }
       }
     })
   }
 
   async update(cartItemId: number, body: UpdateCartItemBodyType): Promise<CartItemType> {
-    await this.validateSKU(body.skuId)
+    await this.validateSKU(body.skuId || 0, body.quantity)
 
     return this.prismaService.cartItem.update({
       where: {
