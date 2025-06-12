8f3ca779424a1246ab134674c51107aa58f46494
Le Duc Thai <leducthai2001@gmail.com>
2025-06-12 18:33:34 +0700

    config redis adapter

--- src/routes/payment/payment.service.ts
+++ src/routes/payment/payment.service.ts
@@ -4,6 +4,7 @@ import { WebhookPaymentBodyType } from 'src/routes/payment/payment.model'
 import { PaymentRepo } from 'src/routes/payment/payment.repo'
 import { SharedWebsocketRepository } from 'src/shared/repositories/shared-websocket.repo'
 import { Server } from 'socket.io'
+import { generateRoomUserId } from 'src/shared/helpers'
 
 @Injectable()
 @WebSocketGateway({ namespace: 'payment' })
@@ -17,17 +18,20 @@ export class PaymentService {
 
   async receiver(body: WebhookPaymentBodyType) {
     const userId = await this.paymentRepo.receiver(body)
+    this.server.to(generateRoomUserId(userId)).emit('payment', {
+      status: 'success'
+    })
 
-    try {
-      const websockets = await this.SharedWebsocketRepository.findMany(userId)
-      websockets.forEach((ws) => {
-        this.server.to(ws.id).emit('payment', {
-          status: 'success'
-        })
-      })
-    } catch (error) {
-      console.log(error)
-    }
+    // try {
+    //   const websockets = await this.SharedWebsocketRepository.findMany(userId)
+    //   websockets.forEach((ws) => {
+    //     this.server.to(ws.id).emit('payment', {
+    //       status: 'success'
+    //     })
+    //   })
+    // } catch (error) {
+    //   console.log(error)
+    // }
     return {
       message: 'Payment successfully'
     }
